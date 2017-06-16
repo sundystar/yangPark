0e7a3c0a8268d02edb8b3ebf4e69bc9b414edb8e
sunliyang <liyang.sun@uebuycar.com>
2017-06-16 19:01:06 +0800

    添加聊天

--- js/chart/index.js
+++ js/chart/index.js
@@ -2,6 +2,61 @@
  * Created by lzy on 17/6/16.
  */
 $(function(){
+
+    var chars = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+    function generateMixed(n) {
+        var res = "";
+        for(var i = 0; i < n ; i ++) {
+            var id = Math.ceil(Math.random()*35);
+            res += chars[id];
+        }
+        return res;
+    }
+
+    var socket = io.connect('http://139.224.13.34:3100');
+    socket.on('connect',function(){
+        socket.emit('connection',{userName:generateMixed(5)})
+    })
+
+    socket.on('init',function(data){
+        var name = data.data;
+        console.log(name);
+    });
+
+    socket.on('message',function(data) {
+
+        var message = data.message;
+
+        var contentme = $("#contentmessage");
+
+        var chatter_convo_left = $("<div class='chatter_convo_left'></div>");
+
+        var chatter_convo_img = $("<div class='chatter_convo_img'></div>");
+        var imgright =$("<img src='file/chart/img/meme.jpeg' />");
+        $(chatter_convo_img).append(imgright);
+        $(chatter_convo_left).append(chatter_convo_img);
+        var content = $("<div class='chatter_convo_content'>"+replace_em(message)+"</div>");
+        $(chatter_convo_left).append(content);
+        var clear = $("<div class='clear'></div>");
+        $(chatter_convo_left).append(clear);
+        $(contentme).append(chatter_convo_left);
+        $('.contentmessage').scrollTop( $('.contentmessage')[0].scrollHeight );
+
+    });
+
+    socket.on('dis',function(data){
+
+    });
+
+
+
+
+
+
+
+
+
     $("#group_search").click(function(){
         $(this).css("display","none");
         $("#group_search_input").css("display","block");
@@ -62,6 +117,7 @@ $(function(){
                 $(chatter_convo_right).append(clear);
                 $(contentme).append(chatter_convo_right);
                 $('.contentmessage').scrollTop( $('.contentmessage')[0].scrollHeight );
+                socket.emit("message",{message:replace_em(this.value)})
             }
             e.preventDefault();
             this.value ='';
